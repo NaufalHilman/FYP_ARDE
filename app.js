c19aabd15028ab826833a4eac4a513fba274c7a7
wishhzz <darywishal125@gmail.com>
2026-06-14 14:52:56 +0800

    update for sql

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const db = require('./config/db');
-const multer = require('multer');
-const fs = require('fs');
+const {uploadResume} = require('./config/cloudinary');
 
-app.use('/uploads', express.static(path.join(__dirname, '../FYP_Admin/public/uploads')));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Test the database connection
 db.query('SELECT 1')
   .then(() => console.log('Database connection successful!'))
@@ -36,18 +35,6 @@ app.get('/about', (req, res) => {
 });
 
 //careers routes
-const resumeStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const dir = path.join(__dirname, '../FYP_Admin/public/uploads/resumes');
-        if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-        cb(null, dir);
-    },
-    filename: (req, file, cb) => {
-        const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, unique + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage: resumeStorage });
 
 app.get('/careers', async (req, res) => {
     try {
@@ -71,10 +58,10 @@ app.get('/careers/:id', async (req, res) => {
 });
 
 // POST /careers/:id/apply
-app.post('/careers/:id/apply', upload.single('resume'), async (req, res) => {
+app.post('/careers/:id/apply', uploadResume.single('resume'), async (req, res) => {
     const { full_name, email, phone, cover_letter } = req.body;
     const career_id = req.params.id;
-    const resume_path = req.file ? '/uploads/resumes/' + req.file.filename : null;
+    const resume_path = req.file ? req.file.path: null;
     try {
         await db.query(
             'INSERT INTO applications (career_id, full_name, email, phone, cover_letter, resume_path) VALUES (?, ?, ?, ?, ?, ?)',
@@ -92,6 +79,11 @@ app.get('/membership', (req, res) => {
     res.render('membership', { active: 'membership' });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
-});
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
